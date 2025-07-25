c627fadb62cda243b38adbc7a0b97a8efbefe81c
EDWINA <edwina.addo@icloud.com>
2025-07-25 15:39:00 +0000

    fixed error p2

--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -1,13 +1,26 @@
 
 import { getProductByCategory } from "@/sanity/vision";
-import React from "react";
 import ProductMap from "@/components/ProductMap";
+import { sanityFetch } from "@/sanity/lib/live";
 
+type Props = {
+  params: {
+    slug: string;
+  };
+};
 
+export async function generateStaticParams() {
+  const categories = await sanityFetch({
+    query: `*[_type == "category"]{ "slug": slug.current }`,
+  });
 
+  return categories.data.map((category: { slug: string }) => ({
+    slug: category.slug,
+  }));
+}
 
-const Page = async  ({ params }: { params: {slug: string } }) => {
-  const {slug} = params;
+const Page = async ({ params }: Props) => {
+  const { slug } = params;
   const product = await getProductByCategory(slug);
 
   return (
@@ -18,7 +31,8 @@ const Page = async  ({ params }: { params: {slug: string } }) => {
         ) : (
           <div className="flex justify-center items-center h-screen">
             <p className="text-gray-500 bg-white text-center font-semibold text-4xl">
-              sorry, no product found for this category for <span className="text-blue-300">{slug}</span>
+              Sorry, no product found for this category:{" "}
+              <span className="text-blue-300">{slug}</span>
             </p>
           </div>
         )}
